b8124dd3806d95ae7674b04047856fe6c673d014
Rinku Bangale <bangalehemant@gmail.com>
2024-06-21 08:41:13 +0000

    added http and other modules

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const app = express();
-// const http = require("http").createServer(app);
-// const path = require("path");
+const http = require("http").createServer(app);
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
 const io = require("socket.io")(http, { cors: { origin: "*" } });
 
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Listening to Port ${PORT}...`);
 });
 
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
-  res.send("index"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 //socket connection
 io.on("connection", (socket) => {
   console.log("user connected: " + socket.id);
-  
+
   //broadcasting users message to all others
   socket.on("message", (payload) => {
     socket.broadcast.emit("message", payload);
